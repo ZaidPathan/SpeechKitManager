cd8c7a1c6cd4f89f3e95fd0f594e516d80d54b16
Zaid Pathan <Zaidkhanintel@gmail.com>
2016-11-28 18:38:59 +0530

    Initial commit

---
+++ Example/SpeechKitManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SpeechKitManager
+//
+//  Created by Zaid Pathan on 11/28/2016.
+//  Copyright (c) 2016 Zaid Pathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
