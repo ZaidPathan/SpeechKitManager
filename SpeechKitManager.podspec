038fd0875a4b9cc5d8366ec1fe8845765bb7e9d5
Zaid Pathan <Zaidkhanintel@gmail.com>
2016-11-28 19:13:00 +0530

    Release 0.0.1

--- SpeechKitManager.podspec
+++ SpeechKitManager.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'SpeechKitManager'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of SpeechKitManager.'
+  s.summary          = 'Development is in progress.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -17,18 +17,15 @@ Pod::Spec.new do |s|
 #   * Write the description between the DESC delimiters below.
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
-
-  s.homepage         = 'https://github.com/<GITHUB_USERNAME>/SpeechKitManager'
+  s.description      = 'Development is in progress. Please wait.'
+  s.homepage         = 'https://github.com/ZaidPathan/SpeechKitManager'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Zaid Pathan' => 'Zaidkhanintel@gmail.com' }
-  s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/SpeechKitManager.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+  s.source           = { :git => 'https://github.com/ZaidPathan/SpeechKitManager.git', :tag => s.version.to_s }
+  # s.social_media_url = 'https://twitter.com/ZaidKhanIntel'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '10.0'
 
   s.source_files = 'SpeechKitManager/Classes/**/*'
   
@@ -37,6 +34,6 @@ TODO: Add long description of the pod here.
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+  # s.frameworks = 'UIKit', 'MapKit','Speech'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
---
+++ SpeechKitManager/Classes/SpeechKitManager.swift
@@ -0,0 +1,137 @@
+//
+//  SpeechKitManager.swift
+//  Created by zaid.pathan on 25/11/16.
+//
+
+import Foundation
+import Speech
+
+public protocol SpeechKitManagerDelegate{
+    // Called when the availability of the given recognizer changes
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool)
+}
+
+private struct SpeechKitManagerErrors {
+    static let recognitionBufferRequestNil = NSError(domain: "recognitionBufferRequest not found", code: 1000, userInfo: nil)
+    static let recognitionURLRequestNil = NSError(domain: "recognitionURLRequest not found", code: 1001, userInfo: nil)
+}
+
+open class SpeechKitManager:NSObject {
+    open var locale = "en_US"   //Ex: for Arabic ar_SA , https://gist.github.com/jacobbubu/1836273
+    
+    open var delegate:SpeechKitManagerDelegate?
+    
+    private lazy var speechRecognizer = SFSpeechRecognizer()
+    
+    //For speech to text
+    private var recognitionBufferRequest:SFSpeechAudioBufferRecognitionRequest?
+    
+    //For audio file to text
+    private var recognitionURLRequest: SFSpeechURLRecognitionRequest?
+    
+    private var audioPlayer:AVAudioPlayer?
+    
+    private lazy var audioEngine = AVAudioEngine()
+    
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    override init() {
+        super.init()
+        speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: locale))
+        speechRecognizer?.delegate = self
+    }
+    
+    //MARK:- Request auth
+    open func requestSpeechRecognizerAuth(_ handler: @escaping (SFSpeechRecognizerAuthorizationStatus) -> Swift.Void){
+        SFSpeechRecognizer.requestAuthorization(handler)
+    }
+    
+    
+    open func recognizeAudio(atURL:URL,resultHandler: @escaping (SFSpeechRecognitionResult?, Error?) -> Swift.Void){
+        speechRecognizer?.delegate = self
+        recognitionURLRequest = SFSpeechURLRecognitionRequest(url: atURL)
+        
+        if let recognitionURLRequest = recognitionURLRequest{
+            speechRecognizer?.recognitionTask(with: recognitionURLRequest, resultHandler: resultHandler)
+        }else{
+            resultHandler(nil, SpeechKitManagerErrors.recognitionURLRequestNil)
+        }
+    }
+    
+    open func recogniseLiveSpeech(resultHandler: @escaping (SFSpeechRecognitionResult?, Error?) -> Swift.Void) throws{
+        // Cancel the previous task if it's running.
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionBufferRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else { fatalError("Audio engine has no input node") }
+        guard let recognitionRequest = recognitionBufferRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        // Configure request so that results are returned before audio recording is finished
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // A recognition task represents a speech recognition session.
+        // We keep a reference to the task so that it can be cancelled.
+        if let recognitionBufferRequest = recognitionBufferRequest{
+            recognitionTask = speechRecognizer?.recognitionTask(with: recognitionBufferRequest, resultHandler: resultHandler)
+        }else{
+            resultHandler(nil, SpeechKitManagerErrors.recognitionBufferRequestNil)
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionBufferRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        try audioEngine.start()
+    }
+    
+    open func record(resultHandler: @escaping (SFSpeechRecognitionResult?, Error?) -> Swift.Void){
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionBufferRequest?.endAudio()
+        } else {
+            try! recogniseLiveSpeech(resultHandler: resultHandler)
+        }
+    }
+    
+    open func stop(){
+        audioEngine.stop()
+        audioEngine.inputNode?.removeTap(onBus: 0)
+        
+        recognitionBufferRequest = nil
+        recognitionTask = nil
+    }
+    
+    //Audio file to text
+    open func playAudio(atURL:URL) throws{
+        audioPlayer = try AVAudioPlayer(contentsOf:atURL)
+        audioPlayer?.prepareToPlay()
+        audioPlayer?.play()
+    }
+    
+    open func pauseAudio(){
+        audioPlayer?.pause()
+    }
+    
+    open func stopAudio(){
+        audioPlayer?.stop()
+    }
+}
+
+
+extension SpeechKitManager:SFSpeechRecognizerDelegate {
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        delegate?.speechRecognizer(speechRecognizer, availabilityDidChange: available)
+    }
+}
